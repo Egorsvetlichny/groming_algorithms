c9a0fb3a22b69b46b3678e9b75be1999b6ab6e93
EgorSvetlichnyy <fkhor@inbox.ru>
2024-05-15 15:32:53 +0300

    improve code quality in graph_Deykstr module

--- graph_Deykstr.py
+++ graph_Deykstr.py
@@ -2,61 +2,57 @@
 def find_lowest_cost_node(costs):
     lowest_cost = float("inf")
     lowest_cost_node = None
+
     for node in costs:
         cost = costs[node]
         if cost < lowest_cost and node not in processed:
             lowest_cost = cost
             lowest_cost_node = node
+
     return lowest_cost_node
 
 
 # Реализация алгоритма Дейкстры
-def alg_Deyk(gr, cos, paren):
+def dijkstra_algorithm(gr, cos, paren):
     node = find_lowest_cost_node(cos)
+
     while node is not None:
         cost = cos[node]
         neighbors = gr[node]
+
         for n in neighbors.keys():
             new_cost = cost + neighbors[n]
             if cos[n] > new_cost:
                 cos[n] = new_cost
                 paren[n] = node
+
         processed.append(node)
         node = find_lowest_cost_node(cos)
+
     return cos["fin"]
 
 
-# Реализация взвешенного графа
-graph = {}
-graph["start"] = {}
-graph["start"]["a"] = 6
-graph["start"]["b"] = 2
-graph["a"] = {}
-graph["a"]["fin"] = 1
-graph["b"] = {}
-graph["b"]["a"] = 3
-graph["b"]["fin"] = 5
-graph["fin"] = {}
-print(graph)
+if __name__ == '__main__':
+    # Реализация взвешенного графа
+    graph = {
+        "start": {"a": 6, "b": 2},
+        "a": {"fin": 1},
+        "b": {"a": 3, "fin": 5},
+        "fin": {}
+    }
 
-# Создание таблицы стоимостей
-infinity = float("inf")
-costs = {}
-costs["a"] = 6
-costs["b"] = 2
-costs["fin"] = infinity
+    # Создание таблицы стоимостей
+    infinity = float("inf")
+    start_costs = {"a": 6, "b": 2, "fin": infinity}
 
-# Создание хеш-таблицы родителей
-parents = {}
-parents["a"] = "start"
-parents["b"] = "start"
-parents["in"] = None
+    # Создание хеш-таблицы родителей
+    parents = {"a": "start", "b": "start", "fin": None}
 
-# Массив для отслеживания всех обработанных узлов
-processed = []
+    # Массив для отслеживания всех обработанных узлов
+    processed = []
 
-print()
+    print(graph)
 
-# Передаём в функцию алгоритма Дейкстры наш подготовленный взвешенный граф,
-# таблицу стоимостей и хеш-таблицу родителей.
-print("Кратчайшее расстояние из узла 'start' в узел 'fin' равно:", alg_Deyk(graph, costs, parents))
+    # Передаём в функцию алгоритма Дейкстры наш подготовленный взвешенный граф,
+    # таблицу стоимостей и хеш-таблицу родителей.
+    print("Кратчайшее расстояние из узла 'start' в узел 'fin' равно:", dijkstra_algorithm(graph, start_costs, parents))
