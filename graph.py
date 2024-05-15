fa68816bc9f107334aac84a947c48ac122cc7996
EgorSvetlichnyy <fkhor@inbox.ru>
2024-05-15 15:37:49 +0300

    improve code quality in graph module

--- graph.py
+++ graph.py
@@ -9,26 +9,31 @@ def search(name):
     search_queue = deque()
     search_queue += graph[name]
     searched = []
+
     while search_queue:
         person = search_queue.popleft()
-        if not person in searched:
+
+        if person not in searched:
             if person_is_seller(person):
                 print(person + ' is a mango seller')
                 return True
             else:
                 search_queue += graph[person]
                 searched.append(person)
+
     return False
 
 
-graph = {}
-graph["you"] = ["alice", "bob", "claire"]
-graph["bob"] = ["anuj", "peggy"]
-graph["alice"] = ["peggy"]
-graph["claire"] = ["thom", "jonny_M"]
-graph["anuj"] = []
-graph["peggy"] = []
-graph["thom"] = []
-graph["jonny"] = []
+if __name__ == '__main__':
+    graph = {
+        "you": ["alice", "bob", "claire"],
+        "bob": ["anuj", "peggy"],
+        "alice": ["peggy"],
+        "claire": ["thom", "jonny_M"],
+        "anuj": [],
+        "peggy": [],
+        "thom": [],
+        "jonny": []
+    }
 
-search('you')
+    search('you')
