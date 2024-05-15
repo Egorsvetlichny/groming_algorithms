29a3b8c01a3fbbd4ffc529eaa908e08614d174c9
EgorSvetlichnyy <fkhor@inbox.ru>
2024-05-15 15:16:20 +0300

    improve code quality in greedy_algo_set module

--- greedy_algo_set.py
+++ greedy_algo_set.py
@@ -2,35 +2,37 @@
 # Программа реализует жадный алгоритм с хорошим, но не оптимальным решением.
 
 # Фукция, покрывающая штаты минимальным набором станций.
-def cover_states_by_stations(st_needed: set(), sts: {}):
+def cover_states(st_needed: set, sts: dict):
     # Хранение итогового набора станций
     final_station = set()
 
     while st_needed:
         best_station = None
         states_covered = set()
+
         for station, states in sts.items():
             covered = st_needed & states
             if len(covered) > len(states_covered):
                 best_station = station
                 states_covered = covered
+
         st_needed -= states_covered
         final_station.add(best_station)
+
     return final_station
 
 
-# Полный список штатов
-states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+if __name__ == '__main__':
+    # Полный список штатов
+    states_needed = {"mt", "wa", "or", "id", "nv", "ut", "ca", "az"}
 
-# Список станций, из которого будет выбираться покрытие
-stations = {
-    "kone": set(["id", "nv", "ut"]),
-    "ktwo": set(["wa", "id", "mt"]),
-    "kthree": set(["or", "nv", "ca"]),
-    "kfour": set(["nv", "ca"]),
-    "kfive": set(["ca", "az"])
-}
+    # Список станций, из которого будет выбираться покрытие
+    stations = {
+        "kone": {"id", "nv", "ut"},
+        "ktwo": {"wa", "id", "mt"},
+        "kthree": {"or", "nv", "ca"},
+        "kfour": {"nv", "ca"},
+        "kfive": {"ca", "or", "az"}
+    }
 
-print("Минимальный набор станций, покрывающий все заданные штаты:", cover_states_by_stations(states_needed, stations))
-
-
+    print("Минимальный набор станций, покрывающий все заданные штаты:", cover_states(states_needed, stations))
